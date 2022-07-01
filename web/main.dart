b0f381e686bff0f71f076b0b323a577642380a40
Qqinzijin <3272362175@qq.com>
2022-07-01 12:25:56 +0000

    新增：改错，更新

--- web/main.dart
+++ web/main.dart
@@ -10,10 +10,10 @@ void main(List<String> args) {
   var chart = context['chart'];
   // 构件js型的变量data
   var data = JsObject.jsify([
-    {'candy': '旺仔牛奶糖', 'sales': 150},
-    {'candy': '大白兔奶糖', 'sales': 100},
-    {'candy': '悠哈', 'sales': 80},
-    {'candy': '玉米糖', 'sales': 60}
+    {'candy': '糖果1', 'sales': 150},
+    {'candy': '糖果2', 'sales': 100},
+    {'candy': '糖果3', 'sales': 80},
+    {'candy': '糖果4', 'sales': 60}
   ]);
 
   // 调用chart的source与render函数向其传入新的数据并使其重新渲染
